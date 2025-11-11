aa4847f3819e227bc2c63bb5e7f6697c140bfcb6
liabenyehonatan <liabenyehonatan@gmail.com>
2025-11-11 12:14:52 +0200

    completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -7,14 +7,15 @@ public class CalcPi {
 		int denominator = 1;
 		double sum = 0;
 
-		for (int i = 0; i <= numOfTerms; i++) {
+		for (int i = 0; i < numOfTerms; i++) {
 			if (i % 2 != 0) {
 				sum = sum - ((double) 1 / denominator);
-				denominator = denominator + 2;
+
 			} else {
 				sum = sum + ((double) 1 / denominator);
-				denominator = denominator + 2;
+
 			}
+			denominator = denominator + 2;
 		}
 		System.out.println("pi according to Java: " + pi);
 		System.out.println("pi, approximated:     " + sum * 4);
